91f11445195db62a516c0e796ff55a7fe4c246c2
Bay5 <farelbayhaqi2544@gmail.com>
2025-11-21 17:12:02 +0700

    firebase setup, auth layout

--- app/src/main/java/com/bay/chatapp/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/bay/chatapp/adapter/ViewPagerAdapter.kt
@@ -3,6 +3,8 @@ package com.bay.chatapp.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.bay.chatapp.view.LoginFragment
+import com.bay.chatapp.view.RegisterFragment
 
 class AuthPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
 
